d703fe06a4f7c8dbfba71e6b5ba0d4beaa16ef77
ayoub khatouri <ayoub.khatouri@usmba.ac.ma>
2023-11-11 23:10:41 +0100

    service customer

--- customer-service/src/main/java/com/example/customerservice/CustomerServiceApplication.java
+++ customer-service/src/main/java/com/example/customerservice/CustomerServiceApplication.java
@@ -1,13 +1,33 @@
 package com.example.customerservice;
 
+import com.example.customerservice.entitites.Customer;
+import com.example.customerservice.repositories.CustomerRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class CustomerServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CustomerServiceApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner commandLineRunner(CustomerRepository customerRepository){
+		return args -> {
+			customerRepository.save(Customer.builder()
+							.firstName("Ayoub")
+							.lastName("Khatouri")
+							.email("ayoub@gmail.com")
+					.build());
+			customerRepository.save(Customer.builder()
+					.firstName("Ali")
+					.lastName("Khatouri")
+					.email("ali@gmail.com")
+					.build());
+		};
 	}
 
 }
