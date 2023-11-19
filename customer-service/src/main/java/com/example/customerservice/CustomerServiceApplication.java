94d64c2fb373b7bfbf81852eb697086cbf874bb2
ayoub khatouri <ayoub.khatouri@usmba.ac.ma>
2023-11-19 03:00:18 +0100

    linking to remote config repo

--- customer-service/src/main/java/com/example/customerservice/CustomerServiceApplication.java
+++ customer-service/src/main/java/com/example/customerservice/CustomerServiceApplication.java
@@ -1,13 +1,16 @@
 package com.example.customerservice;
 
+import com.example.customerservice.config.GlobalConfig;
 import com.example.customerservice.entitites.Customer;
 import com.example.customerservice.repositories.CustomerRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableConfigurationProperties(GlobalConfig.class)
 public class CustomerServiceApplication {
 
 	public static void main(String[] args) {
